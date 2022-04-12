d970b4a22176aedd4cab1990c61561b6b46fed43
Dustin Daves <biginitiald@gmail.com>
2022-04-11 18:03:36 -0700

    Updated Cart functionality, reads Array stored from different pages

--- main.js
+++ main.js
@@ -1,46 +1,83 @@
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
 let shopcartTotal;
 console.log(localStorage.getItem("gTotal"));
+console.log(localStorage.getObj("cartList"));
 document.getElementById("ShopCart").innerText = localStorage.getItem("gTotal");
+
+let shopCartArray;
+shopCartArray = localStorage.getObj("cartList");
+console.log(shopCartArray);
+
+
 
 function ResetCounter(){
 localStorage.setItem("gTotal", 0);
 document.getElementById("ShopCart").innerText = localStorage.getItem("gTotal");
 }
-
-function addToCart(value, page, vegan){
-    console.log(value);
-    console.log(page);
-    console.log(vegan);
-
-    let cartArray = [];
-    cartArray.push( {price: value, pLocal: page, veganOption: vegan} )
-    
-
-    console.log(cartArray[0]);
-    let cartTotal = Number(document.getElementById("ShopCart").innerText);
-    cartTotal = Number(localStorage.getItem("gTotal")) + Number(value);
-    
-    localStorage.setItem("gTotal", cartTotal);
-    localStorage.setItem("cartList", cartArray.push());
-    
-    document.getElementById("ShopCart").innerText = localStorage.getItem("gTotal");
+function ResetArray(){
+    localStorage.setObj("cartList", "")
 }
 
-function checkContents(){
-    let cartItems = localStorage.getItem("cartList");
+function addToCart(buttonData){
+    // console.log(Number(buttonData.value));
+    // console.log(buttonData.dataset.page);
+    // console.log(buttonData.dataset.vegan);
+    let itemCost = buttonData.value;
+    let itemLocal = buttonData.dataset.page;
+    let itemVegan = buttonData.dataset.vegan;
 
-    let list = "<tr><th>Name</th><th>Value</th><th>Item</th></tr>\n";
+    let cartArray = [];
+    let cartArrayStore = localStorage.getObj("cartList");
+    let storeItems = [{'price': itemCost, 'pLocal': itemLocal, 'veganOption': itemVegan}];
+    // cartArrayStore.push(storeItems)
+    console.log(cartArrayStore);
+    // cartArray.push({price: itemCost, pLocal: itemLocal, veganOption: itemVegan});
+    cartArray.push(storeItems[0]);
+    // let cartParse = JSON.parse(cartArray)
 
-    if( cartItems.length == 0){
-        list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td><td><i>empty</i></td></tr>\n";
+    console.log(cartArray[0]);
+    // let showMe = JSON.parse(cartArrayStore);
+    // console.log(showMe);
+    cartArrayStore =  cartArray.concat(cartArrayStore);
+    console.log(cartArrayStore);
+    localStorage.setObj("cartList", cartArrayStore);
+    console.log(localStorage.getObj("cartList"));
+
+    let cartTotal = Number(document.getElementById("ShopCart").innerText);
+    cartTotal = Number(localStorage.getItem("gTotal")) + Number(buttonData.value);
+    
+    localStorage.setItem("gTotal", cartTotal);
+    // localStorage.setItem("cartList", cartArray.push());
+    
+    document.getElementById("ShopCart").innerText = localStorage.getItem("gTotal");
+
+    ShowCart();
+}
+
+function ShowCart(){
+    let cartArrays = localStorage.getObj("cartList");
+    let cartArrayList = cartArrays;
+    console.log(cartArrayList);
+
+    let getCart = cartArrayList;
+    let text = document.getElementById("shopCartTable");
+    var shopRow = "";
+    console.log(getCart);
+
+    for(let i = 0; i < getCart.length; i++){
+        console.log(getCart[i]);
+        shopRow = "<tr>";
+        shopRow += "<td>" + getCart[i].price + "</td>";
+        shopRow += "<td>" + getCart[i].pLocal + "</td>";
+        shopRow += "<td>" + getCart[i].veganOption + "</td></tr>";
     }
-    else{
-        for (i=0; i < cartItems.length; i++){
-            key = cartItems.key(i);
-            let data = JSON.parse(cartItems.getItem(key));
-            list += "<tr><td>" + key + "</td>\n<td>" + data[0] + 
-            "</td>" + data[1] + "</tr>\n";
-        }
-    }
-    console.log(list);
+    console.log(shopRow);
+    
+    text.innerHTML += shopRow;
 }
